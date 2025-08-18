b4ecfd1bc293217d5718e5dbeb06ac686e61bb4e
shrey0303 <shreykhakharia2003@gmail.com>
2025-08-18 10:00:00 +0530

    feat(core): add base result and error types

---
+++ exchange-core/src/main/java/shrey/exchange/BaseResultError.java
@@ -0,0 +1,24 @@
+package shrey.exchange;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResultError extends BaseResult {
+
+    private String message;
+
+    public static final BaseResultError COMMAND_NOT_FOUND = new BaseResultError("Command not found");
+
+    @Override
+    public String toString() {
+        return String.format("%s::%s", 500, message);
+    }
+}
