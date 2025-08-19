ecf0ac823124fbbfc01081a8da76bd311005fcd1
shrey0303 <shreykhakharia2003@gmail.com>
2025-08-19 10:30:00 +0530

    feat(pipeline): implement command buffer event dispatch

---
+++ exchange-core/src/main/java/shrey/exchange/CommandBufferEventDispatcherImpl.java
@@ -0,0 +1,19 @@
+package shrey.exchange;
+
+import com.lmax.disruptor.dsl.Disruptor;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@RequiredArgsConstructor
+public class CommandBufferEventDispatcherImpl implements CommandBufferEventDispatcher {
+
+    private final Disruptor<CommandBufferEvent> commandBufferEventDisruptor;
+
+    @Override
+    public void dispatch(CommandBufferEvent commandBufferEvent) {
+        commandBufferEventDisruptor.publishEvent(((event, sequence) -> event.copy(commandBufferEvent)));
+    }
+}
