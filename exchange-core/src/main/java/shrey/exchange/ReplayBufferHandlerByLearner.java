e21470230ac3096e88383cd3b0dc106ac9a40803
shrey0303 <shreykhakharia2003@gmail.com>
2025-08-29 16:15:00 +0530

    feat(replay): implement learner replay handler

---
+++ exchange-core/src/main/java/shrey/exchange/ReplayBufferHandlerByLearner.java
@@ -0,0 +1,43 @@
+package shrey.exchange;
+
+import shrey.exchange.cluster.LearnerProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@RequiredArgsConstructor
+public class ReplayBufferHandlerByLearner implements ReplayBufferHandler {
+    private final CommandHandler commandHandler;
+    private final StateMachineManager stateMachineManager;
+    private final LearnerProperties learnerProperties;
+
+    @Setter
+    private int eventCount;
+    private Instant lastSnapshot = Instant.now();
+
+    @Override
+    public void onEvent(ReplayBufferEvent event, long sequence, boolean endOfBatch) throws Exception {
+        if (!(event.getCommand() instanceof BaseCommandSnapshot)) {
+            commandHandler.onCommand(event.getCommand());
+        }
+        eventCount--;
+        if (endOfBatch && shouldSnapshot()) {
+            stateMachineManager.takeSnapshot();
+            resetAfterSnapshot();
+        }
+    }
+
+    private boolean shouldSnapshot() {
+        return eventCount < 0 || Instant.now().compareTo(lastSnapshot.plus(learnerProperties.getSnapshotLifeTime())) > 0;
+    }
+
+    private void resetAfterSnapshot() {
+        eventCount = learnerProperties.getSnapshotFragmentSize();
+        lastSnapshot = Instant.now();
+    }
+}
