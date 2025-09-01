9d16e34e5c3836662c507545f4f1b8d22cdb162e
shrey0303 <shreykhakharia2003@gmail.com>
2025-09-01 10:30:00 +0530

    feat(cluster): implement leader node bootstrap with Disruptor startup sequence

---
+++ exchange-core/src/main/java/shrey/exchange/cluster/LeaderProperties.java
@@ -0,0 +1,18 @@
+package shrey.exchange.cluster;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeaderProperties {
+    private int commandBufferSize = 1 << 15;
+    private int replyBufferSize = 1 << 16;
+    private int logsChunkSize = 200;
+}
