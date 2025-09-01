eb010ef8f5e7bfda3908b98470dfba881c90feca
shrey0303 <shreykhakharia2003@gmail.com>
2025-09-01 16:45:00 +0530

    chore(core): configure exchange-core module dependencies

---
+++ exchange-core/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java-library-conventions")
+}
+
+dependencies {
+    implementation(libs.slf4j)
+    implementation(libs.logback)
+    implementation(libs.kafkaClient)
+    implementation(libs.lmaxDisruptor)
+    implementation(libs.agrona)
+    implementation("org.hdrhistogram:HdrHistogram:2.1.12")
+    implementation(project(":exchange-libs:common"))
+    implementation(project(":exchange-libs:exchange-proto"))
+    compileOnly("org.projectlombok:lombok:${libs.versions.lombokVersion.get()}")
+    annotationProcessor("org.projectlombok:lombok:${libs.versions.lombokVersion.get()}")
+
+    runtimeOnly("io.grpc:grpc-netty-shaded:${libs.versions.grpcVersion.get()}")
+    implementation("io.grpc:grpc-services:${libs.versions.grpcVersion.get()}")
+    implementation("io.grpc:grpc-protobuf:${libs.versions.grpcVersion.get()}")
+    implementation("io.grpc:grpc-stub:${libs.versions.grpcVersion.get()}")
+    compileOnly("org.apache.tomcat:annotations-api:${libs.versions.annotationsApiVersion.get()}")
+    implementation("com.google.protobuf:protobuf-java-util:${libs.versions.protocVersion.get()}")
+}
+
+@Suppress("DEPRECATION")
+val generatedDir = file("${buildDir}/generated/src/main/java")
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/java", generatedDir)
+    }
+}
