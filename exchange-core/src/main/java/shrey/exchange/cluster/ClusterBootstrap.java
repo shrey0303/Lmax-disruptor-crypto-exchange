1a6b182aa5afe8c7fac6621ffe90281258680905
shrey0303 <shreykhakharia2003@gmail.com>
2025-09-01 09:15:00 +0530

    feat(cluster): add cluster bootstrap interface, roles, and status tracking

---
+++ exchange-core/src/main/java/shrey/exchange/cluster/ClusterBootstrap.java
@@ -0,0 +1,10 @@
+package shrey.exchange.cluster;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+public interface ClusterBootstrap {
+    void onStart();
+    void onStop();
+}
---
+++ exchange-core/src/main/java/shrey/exchange/cluster/ClusterRole.java
@@ -0,0 +1,11 @@
+package shrey.exchange.cluster;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+public enum ClusterRole {
+    LEADER,
+    FOLLOWER,
+    LEARNER
+}
