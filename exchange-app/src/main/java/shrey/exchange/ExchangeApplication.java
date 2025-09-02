01f75c71b107ba85848a921c2deba80b6d22b602
shrey0303 <shreykhakharia2003@gmail.com>
2025-09-02 09:50:00 +0530

    feat(app): add Spring Boot main application class

---
+++ exchange-app/src/main/java/shrey/exchange/ExchangeApplication.java
@@ -0,0 +1,16 @@
+package shrey.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@SpringBootApplication
+public class ExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeApplication.class, args);
+    }
+}
