9c59ff122ad456fe54b63530fb6f6ac4ee2483ce
shrey0303 <shreykhakharia2003@gmail.com>
2025-12-08 10:45:00 +0530

    feat(benchmark): add base reply model

---
+++ benchmark/benchmark-cluster/src/main/java/shrey/benchmarkcluster/common/BaseReply.java
@@ -0,0 +1,20 @@
+package shrey.benchmarkcluster.common;
+
+import lombok.Data;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author shrey
+ * @since 2024
+ */
+@Data
+public class BaseReply {
+    public BaseReply(long startTime) {
+        this.startTime = startTime;
+        this.future = new CompletableFuture<>();
+    }
+    private CompletableFuture<Long> future;
+    private long startTime;
+    private long endTime;
+}
